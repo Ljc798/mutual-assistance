2f3d458d9e6c8499912b81fbdf721a62af66d046
Ljc798 <loujiachen6@gmail.com>
2025-09-11 15:36:39 +0800

    Update redis.js

--- server/utils/redis.js
+++ server/utils/redis.js
@@ -5,7 +5,6 @@ const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT || 6379,
   password: process.env.REDIS_PASSWORD,
-  db: process.env.REDIS_DB || 0,
   // 限制连接重试次数，防止一直挂
   maxRetriesPerRequest: 3,
   connectTimeout: 10000, // 10s 超时
